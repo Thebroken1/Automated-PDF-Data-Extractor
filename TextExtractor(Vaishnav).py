faf31b833342beb1d74a06ca22e715c2177077bc
VS <vaish261@gmail.com>
2025-03-07 18:16:03 +0400

    text Changes

--- TextExtractor(Vaishnav).py
+++ TextExtractor(Vaishnav).py
@@ -6,11 +6,9 @@ pdf_path = "killme.pdf"
 # Try PyPDF2 first
 reader = PdfReader(pdf_path)
 
-for page in reader.pages:
-    print(page.extract_text())
+text = "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])
 
 with pdfplumber.open(pdf_path) as pdf:
     text = "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
 
-
-'''text = "\n".join([page.extract_text() for page in reader.pages if page.extract_text()])'''
+print(text)
